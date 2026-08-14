36193c6886300c8eaa21957e6a15abc25442e4f3
mirjunaidulislam21-ship-it <mirjunaidulislam21@gmail.com>
2026-08-14 12:40:49 +0000

    Add Java Maven application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from My-first-cicd-project!");
+        System.out.println("CI/CD pipeline is working.");
+    }
+}
